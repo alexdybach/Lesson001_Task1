765356c2d39b39151694bf0032193c58feb55ed2
Олександр Дибач <alex.dybach.2014@i.ua>
2020-10-22 20:17:53 +0300

    done lesson 1 task 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            byte b1 = 117;
+            sbyte sb1 = -83;
+            ushort ush1 = 58302;
+            short sh = -15832;
+            uint ui1 = 3974619u;
+            int int1 = -2387465;
+            ulong ul1 = 74569234875169847;
+            long lg1 = -13781735813;
+            Console.WriteLine(b1+"\t"+sb1 + "\t" +ush1 + "\t" +sh + "\t" +ui1 + "\t" +int1 + "\t" +ul1 + "\t" +lg1);
+
+            #endregion
+        }
+    }
+}
